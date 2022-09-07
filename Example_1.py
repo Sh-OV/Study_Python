abd390771dae488a8441f30309ce102ab6714e0a
Sh-OV <shchegolga@yandex.ru>
2022-09-07 20:33:42 +0500

    Первый семинар, первая функция ))

--- Example_1.py
+++ Example_1.py
@@ -1,2 +1,9 @@
-import pandas
-
+num_1 = int(input('Введите первое число: '))
+num_2 = int(input('Введите второе число: '))
+print(type(num_1))
+if (num_2 == (num_1 ** 2)):
+    print(True)
+elif (num_1 == (num_2 ** 2)):
+    print(True)
+else: 
+    print(False)
