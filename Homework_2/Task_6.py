7b6bb5b712fe570aacee0df3a83e7fb0e8436d2b
Sh-OV <shchegolga@yandex.ru>
2022-09-08 20:41:13 +0500

    Д/з задача на сумму цифр вещественного числа

---
+++ Homework_2/Task_6.py
@@ -0,0 +1,17 @@
+# Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+# Пример:
+# - 0,56 -> 11
+number = (input('Введите вещественное число: '))
+len_num = len(number.split('.')[1])
+def Sum_digits (num, quant):
+    num = float(num)
+    num *= 10 ** quant
+    num = int(num)
+    count = 0
+    sum = 0
+    while (num > 0):
+        sum += num % 10
+        num //= 10
+    return sum
+rezult = Sum_digits(number, len_num)
+print(rezult)
