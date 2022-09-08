473fb96940b1d2a51a0c142d7ecbbc5b69404b74
Sh-OV <shchegolga@yandex.ru>
2022-09-08 20:58:29 +0500

    Д/з задача на сумму цифр вещественного числа, добавила проверку на ввод числа

--- Homework_2/Task_6.py
+++ Homework_2/Task_6.py
@@ -2,13 +2,14 @@
 # Пример:
 # - 0,56 -> 11
 number = (input('Введите вещественное число: '))
+if (float(number) % 1 == 0.0):
+    print("Вы ввели целое, а не вещественное число!")
+    print(" ")
 len_num = len(number.split('.')[1])
 def Sum_digits (num, quant):
     num = float(num)
     num *= 10 ** quant
     num = int(abs(num))
-    
-    count = 0
     sum = 0
     while (num > 0):
         sum += num % 10
