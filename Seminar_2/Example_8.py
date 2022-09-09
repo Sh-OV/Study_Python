213a1c4983765306597efce8125cf0d0eed0e33e
Sh-OV <shchegolga@yandex.ru>
2022-09-09 20:52:24 +0500

    Добавила решение срезом списка через цикл while

--- Seminar_2/Example_8.py
+++ Seminar_2/Example_8.py
@@ -1,6 +1,8 @@
 # Напишите программу, в которой пользователь будет задавать две строки,
 # а программа - определять количество вхождений одной строки в другой
 
+print('Решение № 1: ')
+
 line_1 = input('Введите первую стрку: ')
 line_2 = input('Введите вторую стрку: ')
 
@@ -10,3 +12,16 @@ def Function_counting_occurrences(l1, l2):
 
 counting_occurrences = Function_counting_occurrences(line_1, line_2)
 print(counting_occurrences)
+
+print('Решение № 2: ')
+
+counter = 0
+count = 0
+len_1 = len(line_1)
+len_2 = len(line_2)
+
+while count < len_1:
+    if (line_1[count : (count + len_2)] == line_2):
+        counter += 1
+    count += 1
+print(counter)
