a817b03f90e1986d5d42dc6a6b56fd924e1ca20b
Sh-OV <shchegolga@yandex.ru>
2022-09-09 17:44:50 +0500

    Задача по перемешиванию списка

--- Homework_2/Task_10.py
+++ Homework_2/Task_10.py
@@ -1,5 +1,9 @@
 # Реализуйте алгоритм перемешивания списка.
 
+from random import Random
+import random
+
+
 number = int(input('Введите количество элементов списка: '))
 
 def Function_list_creation(num):
@@ -12,3 +16,10 @@ def Function_list_creation(num):
         
 list_creation = Function_list_creation(number)
 print(list_creation)    
+
+def function_list_shuffle (m_list):
+    random.shuffle(m_list)
+    return m_list
+    
+list_shuffle = function_list_shuffle (list_creation)
+print(list_shuffle)
