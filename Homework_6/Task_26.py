6403cb2522c51994216fe3bdb6051ff8179f4baa
Sh-OV <shchegolga@yandex.ru>
2022-10-07 12:54:01 +0500

    Решение д.з 4, задача 17 с помощью ускоренной обработки данных

---
+++ Homework_6/Task_26.py
@@ -0,0 +1,15 @@
+# Реализуйте алгоритм перемешивания списка.
+
+from random import Random
+import random
+
+
+number = int(input('Введите количество элементов списка: '))
+list_creation = [i for i in range(0, number + 1)]
+print(list_creation)   
+random.shuffle(list_creation)
+print(list_creation)
+
+
+        
+
