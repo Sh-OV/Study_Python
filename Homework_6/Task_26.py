ff410ecb71d470aa21ce3eda7fd2d336071c27ba
Sh-OV <shchegolga@yandex.ru>
2022-10-07 13:34:06 +0500

    Решение д.з 2, задача 10 с помощью ускоренной обработки данных

--- Homework_6/Task_26.py
+++ Homework_6/Task_26.py
@@ -3,13 +3,10 @@
 from random import Random
 import random
 
-
 number = int(input('Введите количество элементов списка: '))
 list_creation = [i for i in range(0, number + 1)]
 print(list_creation)   
 random.shuffle(list_creation)
 print(list_creation)
 
-
-        
 
