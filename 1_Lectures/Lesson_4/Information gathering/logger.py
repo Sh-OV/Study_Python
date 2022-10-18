096217b4a2bcc361b09bbd74ff853968e3ea2c46
Sh-OV <shchegolga@yandex.ru>
2022-10-18 21:40:01 +0500

    Перенесла файл log_csv и сделала ссылку на него в файле logger

--- 1_Lectures/Lesson_4/Information gathering/logger.py
+++ 1_Lectures/Lesson_4/Information gathering/logger.py
@@ -1,22 +1,24 @@
 from datetime import datetime as dt
 
+path = '1_Lectures\Lesson_4\Information gathering\log_csv'
+
 def temperature_logger(data):
     time = dt.now().strftime('%H:%M')
-    with open('log_csv', 'a') as file:
+    with open(path, 'a') as file:
         file.write('{}; temperature; {}\n'
                     .format(time, data))
     return
 
 def pressure_logger(data):
     time = dt.now().strftime('%H:%M')
-    with open('log_csv', 'a') as file:
+    with open(path, 'a') as file:
         file.write('{}; pressure; {}\n'
                     .format(time, data))
     return
 
 def wind_speed_logger(data):
     time = dt.now().strftime('%H:%M')
-    with open('log_csv', 'a') as file:
+    with open(path, 'a') as file:
         file.write('{}; wind_speed; {}\n'
                     .format(time, data))
     return
