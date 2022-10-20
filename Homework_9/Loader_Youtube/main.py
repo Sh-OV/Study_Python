edb76d7fffda5bfba34a0d7dbbe2d83f4f1cb441
Sh-OV <shchegolga@yandex.ru>
2022-10-20 07:55:05 +0500

    Загрузчик видео и аудио с YouTube

---
+++ Homework_9/Loader_Youtube/main.py
@@ -0,0 +1,13 @@
+from pytube import YouTube 
+
+link = input('Введите ссылку на видео с Youtube: ')
+vid = YouTube(link)
+print('Наименование загружаемого файла: ', vid.title)
+print('Просмотры: ', vid.views)
+print('Длина трека: ', vid.length, 'секунд')
+print('Описание: ', vid.description)
+print('Рейтинги: ', vid.rating)
+vid_download = vid.streams.get_by_itag('17')
+vid_download.download("C:\SAVE_PATH")
+print('Скачивание завешено!')
+
